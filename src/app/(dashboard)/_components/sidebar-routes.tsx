9bed70f45c5e305e2ca5414cfa2909615485be7d
ftessmann <fernando.tessmann@gmail.com>
2024-05-17 14:39:21 -0300

    created new reusable components

--- src/app/(dashboard)/_components/sidebar-routes.tsx
+++ src/app/(dashboard)/_components/sidebar-routes.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { 
+    BarChart,
     Compass, 
-    Layout } 
-from 'lucide-react';
+    Layout, 
+    List
+} from 'lucide-react';
 
 import { SidebarItem } from './sidebar-item';
+import { usePathname } from 'next/navigation';
 
 const guestRoutes = [{
     icon: Layout,
@@ -17,11 +20,27 @@ const guestRoutes = [{
     label: 'Search',
     href: '/search'
 },
-]
+];
+
+const teacherRoutes = [{
+    icon: List,
+    label: 'Courses',
+    href: '/teacher/courses'
+},
+{
+    icon: BarChart,
+    label: 'Analytics',
+    href: '/teacher/analytics'
+},
+];
 
 export const SidebarRoutes = () => {
 
-    const routes = guestRoutes;
+    const pathname = usePathname();
+
+    const isTeacherPage = pathname?.includes('/teacher')
+
+    const routes = isTeacherPage ? teacherRoutes : guestRoutes;
 
     return (
         <div className='flex flex-col w-full'>
