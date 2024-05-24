804059b1813ab33f95e3a0c0caef74f9ba5fd280
ftessmann <fernando.tessmann@gmail.com>
2024-05-24 13:47:53 -0300

    added attachment form

--- src/app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ src/app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -2,12 +2,13 @@ import { IconBadge } from "@/components/icon-bagde";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
-import { CircleDollarSign, LayoutDashboard, ListChecks } from "lucide-react";
+import { CircleDollarSign, File, LayoutDashboard, ListChecks } from "lucide-react";
 import { TitleForm } from "./_components/title-form";
 import { DescriptionForm } from "./_components/description-form";
 import { ImageForm } from "./_components/image-form";
 import { CategoryForm } from "./_components/category-form";
 import { PriceForm } from "./_components/price-form";
+import { AttachmentForm } from "./_components/attachment-form";
 
 const CourseIdPage = async ({
     params
@@ -24,7 +25,14 @@ const CourseIdPage = async ({
     const course = await db.course.findUnique({
         where: {
             id: params.courseId
-        }
+        },
+        include: {
+            attachments: {
+                orderBy: {
+                    createdAt: 'desc',
+                },
+            },
+        },
     });
 
     const categories = await db.category.findMany({
@@ -132,9 +140,25 @@ const CourseIdPage = async ({
                         courseId={course.id}
                     />
 
+                    <div className='flex items-center gap-x-2'>
+
+                        <IconBadge icon={File} />
+
+                        <h2 className='text-xl'>
+                            Attachments & Resources
+                        </h2>
+
+                    </div>
+
+                    <AttachmentForm 
+                        initialData={course}
+                        courseId={course.id}
+                    />
+
                 </div>
 
             </div>
+
         </div>
     )
 };
