d8b40f1b7d64e72d993889ac926eec8a12c13b19
ftessmann <fernando.tessmann@gmail.com>
2024-07-11 11:25:07 -0300

    typo

--- src/app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-access-form.tsx
+++ src/app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-access-form.tsx
@@ -70,7 +70,7 @@ export const ChapterAccessForm = ({
     return (
         <div className='mt-6 border bg-slate-100 rounded-md p-4'>
             <div className='font-medium flex items-center justify-between'>
-                Chapter access
+                Chapter Access
 
                 <Button 
                     variant='ghost'
