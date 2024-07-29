cfe14ac2a82e87b8c8ae96b63e8d97da4512c243
ftessmann <fernando.tessmann@gmail.com>
2024-07-29 16:40:18 -0300

    feat: category item component

--- src/app/(dashboard)/(routes)/search/_components/categories.tsx
+++ src/app/(dashboard)/(routes)/search/_components/categories.tsx
@@ -15,6 +15,7 @@ import {
     FcCloth
 } from "react-icons/fc"
 import { IconType } from "react-icons/lib";
+import { CategoryItem } from "./category-item";
 
 interface CategoriesProps {
     items: Category[]
@@ -37,8 +38,16 @@ export const Categories = ({
     items
 }: CategoriesProps ) => {
     return (
-        <div>
-
+        <div className='flex items-center gap-x-2 overflow-x-auto pb-2'>
+            
+            {items.map((item) => (
+                <CategoryItem 
+                    key={item.id}
+                    label={item.name}
+                    icon={iconMap[item.name]}
+                    value={item.id}
+                />
+            ))}
         </div>
     )
 };
