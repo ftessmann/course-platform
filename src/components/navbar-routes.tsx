5b3728eb59044659daf24dec2cf593c9c5d2c539
ftessmann <fernando.tessmann@gmail.com>
2024-08-30 10:15:43 -0300

    feat: navbar routes component

--- src/components/navbar-routes.tsx
+++ src/components/navbar-routes.tsx
@@ -12,7 +12,7 @@ export const NavbarRoutes = () => {
     const pathname = usePathname();
     
     const isTeacherPage = pathname?.startsWith('/teacher');
-    const isPlayerPage = pathname?.includes('/chapter');
+    const isCoursePage = pathname?.includes('/courses');
     const isSearchPage = pathname === "/search"
 
     return (
@@ -23,7 +23,7 @@ export const NavbarRoutes = () => {
                 </div>
             )}
             <div className='flex gap-x-2 ml-auto'>
-                {isTeacherPage || isPlayerPage ? (
+                {isTeacherPage || isCoursePage ? (
                     <Link href='/'>
                         <Button
                             size='sm'
